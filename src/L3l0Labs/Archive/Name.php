a812e4b8e2301f7bcd3c0d710d65f0be748c8e73
l3l0 <leszek.prabucki@gmail.com>
2014-04-04 21:06:56 +0200

    Add Archive name value object

---
+++ src/L3l0Labs/Archive/Name.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace L3l0Labs\Archive;
+
+class Name
+{
+    private $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+    public static function create($name)
+    {
+        return new self($name);
+    }
+}
